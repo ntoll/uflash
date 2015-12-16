34f8c8a9849e8eba6450bf3c474729d3de7189ab
Nicholas H.Tollervey <ntoll@ntoll.org>
2015-12-16 21:33:40 +0000

    Add basic (untested) setup.py and CHANGES.rst.

---
+++ CHANGES.rst
@@ -0,0 +1,7 @@
+Release History
+---------------
+
+0.0.1
++++++
+
+Initial release. Basic functionality.
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+with open('README.rst') as f:
+    readme = f.read()
+with open('CHANGES.rst') as f:
+    changes = f.read()
+
+
+setup(
+    name='gordon',
+    version='0.0.1',
+    description='A module and utility to flash Python onto the BBC micro:bit.',
+    long_description=readme + '\n\n' + changes,
+    author='Nicholas H.Tollervey',
+    author_email='ntoll@ntoll.org',
+    url='http://micropython.org/',
+    package_dir={'gordon': 'gordon'},
+    package_data={'': ['firmware.hex', 'README.rst', 'CHANGES.rst', 'LICENSE',
+                       'AUTHORS']},
+    license='MIT',
+)
