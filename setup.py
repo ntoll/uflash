28c3203d68507b46297952b103e1c684b3222c6c
Nicholas H.Tollervey <ntoll@ntoll.org>
2015-12-21 00:28:12 +0000

    Bump version and update setup.

--- setup.py
+++ setup.py
@@ -9,15 +9,14 @@ with open('CHANGES.rst') as f:
 
 setup(
     name='uflash',
-    version='0.9.0',
+    version='0.9.1',
     description='A module and utility to flash Python onto the BBC micro:bit.',
     long_description=readme + '\n\n' + changes,
     author='Nicholas H.Tollervey',
     author_email='ntoll@ntoll.org',
     url='https://github.com/ntoll/uflash',
     package_dir={'uflash': 'uflash'},
-    package_data={'': ['firmware.hex', 'README.rst', 'CHANGES.rst', 'LICENSE',
-                       'AUTHORS']},
+    package_data={'': ['firmware.hex', 'README.rst', 'CHANGES.rst',]},
     license='MIT',
     classifiers=[
         'Development Status :: 4 - Beta',
