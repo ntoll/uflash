275bbb2674fd486ed301826ecf736404935bf991
Scott Webster <ScottDWebster@optimum.net>
2018-06-18 18:23:20 -0400

    Created new hexify script
    New script allows for multiple input files.

---
+++ hexify.py
@@ -0,0 +1,31 @@
+import uflash
+import argparse
+import sys
+import os
+
+_HELP_TEXT = """
+Creates hexified versions of micropython scripts.
+Intended for saving files to the local filesystem, _NOT_ the microbit.
+Does not autodetect a microbit.
+Accepts multiple imput scripts and optionally one output directory.
+"""
+
+def main(argv=None):
+    if not argv:
+            argv = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(description=_HELP_TEXT)
+    parser.add_argument('source', nargs='*', default=None)
+    parser.add_argument('-o', '--outdir', default=None,
+                            help="Output directory")
+    args = parser.parse_args(argv)
+    
+    for file in args.source:
+        if not args.outdir:
+            (script_path, script_name) = os.path.split(file)
+            args.outdir = script_path
+        uflash.flash(path_to_python=file,
+                paths_to_microbits=[args.outdir], keepname=True)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
