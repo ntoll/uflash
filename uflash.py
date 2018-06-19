1bc638031d9b78099c9a71dba39276281db1c502
Scott Webster <ScottDWebster@optimum.net>
2018-06-19 15:09:56 -0400

    Fix double printing message
    Properly wrapped message prints in conditionals to avoid double
    printing.

--- uflash.py
+++ uflash.py
@@ -337,8 +337,9 @@ def flash(path_to_python=None, paths_to_microbits=None,
             else:
                 hex_path = os.path.join(path, 'micropython.hex')
             if path_to_python:
-                print('Flashing {} to: {}'.format(script_name, hex_path))
-                if keepname:
+                if not keepname:
+                    print('Flashing {} to: {}'.format(script_name, hex_path))
+                else:
                     print('Hexifying {} as: {}'.format(script_name, hex_path))
             else:
                 print('Flashing Python to: {}'.format(hex_path))
