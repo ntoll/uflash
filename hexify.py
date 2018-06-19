1bbaae7cc424e3573f4ed95650d818e7859000a5
Scott Webster <ScottDWebster@optimum.net>
2018-06-19 15:57:38 -0400

    Attempting to reduce "make pep8" noise
    Changed Makefile to call pycodestyle instead of pep8.
    Changed continuation line indentation in hexify.py.
    Changed test_uflash.py to have two and only two blank lines between
    function defs.

--- hexify.py
+++ hexify.py
@@ -9,6 +9,7 @@ scripts on the local filesystem _NOT_ the microbit.  Does not autodetect a
 microbit.  Accepts multiple input scripts and optionally one output directory.
 """
 
+
 def main(argv=None):
     if not argv:
             argv = sys.argv[1:]
@@ -16,15 +17,16 @@ def main(argv=None):
     parser = argparse.ArgumentParser(description=_HELP_TEXT)
     parser.add_argument('source', nargs='*', default=None)
     parser.add_argument('-o', '--outdir', default=None,
-                            help="Output directory")
+                help="Output directory")
     args = parser.parse_args(argv)
-    
+
     for file in args.source:
         if not args.outdir:
             (script_path, script_name) = os.path.split(file)
             args.outdir = script_path
         uflash.flash(path_to_python=file,
-                paths_to_microbits=[args.outdir], keepname=True)
+                    paths_to_microbits=[args.outdir], keepname=True)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
